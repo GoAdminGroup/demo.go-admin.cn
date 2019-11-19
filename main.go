3346efabefe6277a6784274eaa76d53ad7b1d1dc
cg33 <chg80333@163.com>
2019-11-20 06:05:02 +0800

    add missing theme import

--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
 	_ "github.com/GoAdminGroup/go-admin/adapter/gin"
 	_ "github.com/GoAdminGroup/go-admin/modules/db/drivers/mysql"
 	"github.com/GoAdminGroup/go-admin/template/chartjs"
+	_ "github.com/GoAdminGroup/themes/adminlte"
 	_ "github.com/GoAdminGroup/themes/sword"
 
 	"github.com/GoAdminGroup/demo/login"
