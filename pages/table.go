1f2305ee883fa4bcdc5fae944489ca390916ff21
chenhg5 <chg80333@163.com>
2024-04-25 11:36:41 +0800

    update

--- pages/table.go
+++ pages/table.go
@@ -12,7 +12,9 @@ import (
 
 func GetTableContent(ctx *gin.Context) (types.Panel, error) {
 
-	comp := template2.Get(context.NewContext(ctx.Request), config.GetTheme())
+	newCtx := context.NewContext(ctx.Request)
+
+	comp := template2.Get(newCtx, config.GetTheme())
 
 	table := comp.DataTable().
 		SetInfoList([]map[string]types.InfoItem{
@@ -45,7 +47,7 @@ func GetTableContent(ctx *gin.Context) (types.Panel, error) {
 			SetNoPadding().
 			SetHeader(table.GetDataTableHeader()).
 			WithHeadBorder().
-			SetFooter(paginator.Get(&context.Context{}, paginator.Config{
+			SetFooter(paginator.Get(newCtx, paginator.Config{
 				Size:         50,
 				PageSizeList: []string{"10", "20", "30", "50"},
 				Param:        parameter.GetParam(ctx.Request.URL, 10),
